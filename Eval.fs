c486a832a5276f7e49dce5bb507c4768d8f52988
Marco Nardelotto <marco.nardelotto@gmail.com>
2022-02-09 21:34:31 +0100

    Aggiungere i file di progetto.

---
+++ Eval.fs
@@ -0,0 +1,121 @@
+﻿(*
+* TinyML
+* Eval.fs: evaluator
+*)
+
+module TinyML.Eval
+
+open Ast
+
+let rec eval_expr (env : value env) (e : expr) : value =
+    match e with
+    | Lit lit -> VLit lit
+
+    | Var x ->
+        let _, v = List.find (fun (y, _) -> x = y) env
+        v
+
+    | Lambda (x, _, e) -> Closure (env, x, e)
+
+    | App (e1, e2) ->
+        let v1 = eval_expr env e1
+        let v2 = eval_expr env e2
+        match v1 with
+        | Closure (env1, x, e) -> eval_expr ((x, v2) :: env1) e
+        | RecClosure (env1, f, x, e) -> eval_expr ((x, v2) :: (f, v1) :: env1) e
+        | _ -> unexpected_error "eval_expr: non-closure in left side of application: %s" (pretty_value v1)
+        
+    | IfThenElse (e1, e2, None) ->
+        let v1 = eval_expr env e1
+        match v1 with
+        | VLit (LBool true) -> eval_expr env e2
+        | VLit (LBool false) -> VLit LUnit
+        | _ -> unexpected_error "eval_expr: non-boolean in if guard: %s" (pretty_value v1)
+        
+
+    | IfThenElse (e1, e2, Some e3) ->
+        let v1 = eval_expr env e1
+        eval_expr env (match v1 with
+                       | VLit (LBool true) -> e2
+                       | VLit (LBool false) -> e3
+                       | _ -> unexpected_error "eval_expr: non-boolean in if guard: %s" (pretty_value v1)
+                       )
+
+    | Let (x, _, e1, e2) -> 
+        let v1 = eval_expr env e1
+        eval_expr ((x, v1) :: env) e2
+
+     // TODO check this implementation
+    | LetRec (f, _, e1, e2) -> 
+        let v1 = eval_expr env e1
+        let clos = match v1 with
+        | Closure (venv1, x, e) -> RecClosure (venv1, f, x, e)
+        | _ -> unexpected_error "eval_expr: expected closure in rec binding but got: %s" (pretty_value v1)
+        eval_expr ((f,clos) :: env) e2
+
+    //TODO: Tuple impl
+    | Tuple es ->
+        VTuple (List.map (eval_expr env) es)
+
+    //BinOP ( + , - , * , / , % )
+    | BinOp (e1, "+", e2) -> binop (+) (+) "+" env e1 e2
+    | BinOp (e1, "-", e2) -> binop (-) (-) "-" env e1 e2
+    | BinOp (e1, "*", e2) -> binop ( * ) ( * ) "*" env e1 e2
+    | BinOp (e1, "/", e2) -> binop ( / ) ( / ) "/" env e1 e2
+    | BinOp (e1, "%", e2) -> binop ( % ) ( % ) "%" env e1 e2
+
+    //BinOP ( < , <= , > , >= , = , <> )
+    | BinOp (e1, "<", e2) -> binop_compare (<) (<) "<" env e1 e2
+    | BinOp (e1, "<=", e2) -> binop_compare (<=) (<=) "<=" env e1 e2
+    | BinOp (e1, ">", e2) -> binop_compare (>) (>) ">" env e1 e2
+    | BinOp (e1, ">=", e2) -> binop_compare (>=) (>=) ">=" env e1 e2
+    | BinOp (e1, "=", e2) -> binop_compare (=) (=) "=" env e1 e2
+    | BinOp (e1, "<>", e2) -> binop_compare (<>) (<>) "<>" env e1 e2
+    
+    //BinOP ( and , or )
+    | BinOp (e1, "and", e2) -> binop_logic (&&) "and" env e1 e2
+    | BinOp (e1, "or", e2) -> binop_logic (||) "and" env e1 e2
+
+    //UnOp ( not , - )
+    | UnOp ("not" , e1) ->
+         let v1 = eval_expr env e1
+         match v1 with
+         | VLit (LBool b) -> VLit (LBool (not b))
+         | _ -> unexpected_error "eval_expr: illegal operands in unary operator (%s): %s %s" "not" "not" (pretty_value v1)
+
+    | UnOp ("-" , e1) ->
+        let v1 = eval_expr env e1
+        match v1 with
+        | VLit (LInt i) -> VLit (LInt (- i))
+        | VLit (LFloat f) -> VLit (LFloat (- f))
+        | _ -> unexpected_error "eval_expr: illegal operands in unary operator (%s): %s %s" "-" "-" (pretty_value v1)
+
+    | _ -> unexpected_error "eval_expr: unsupported expression: %s [AST: %A]" (pretty_expr e) e
+
+    //TODO: check repetitions
+and binop op_int op_float op env e1 e2 =
+    let v1 = eval_expr env e1
+    let v2 = eval_expr env e2
+    match v1, v2 with
+    | VLit (LInt x), VLit (LInt y) -> VLit (LInt (op_int x y))
+    | VLit (LFloat x), VLit (LFloat y) -> VLit (LFloat (op_float x y))
+    | VLit (LInt x), VLit (LFloat y) -> VLit (LFloat (op_float (float x) y))
+    | VLit (LFloat x), VLit (LInt y) -> VLit (LFloat (op_float x (float y)))
+    | _ -> unexpected_error "eval_expr: illegal operands in binary operator (%s): %s %s %s" (op) (pretty_value v1) (op) (pretty_value v2)
+
+and binop_compare op_int op_float op env e1 e2 =
+    let v1 = eval_expr env e1
+    let v2 = eval_expr env e2
+    match v1, v2 with
+    | VLit (LInt x), VLit (LInt y) -> VLit (LBool (op_int x y))
+    | VLit (LFloat x), VLit (LFloat y) -> VLit (LBool (op_float x y))
+    | VLit (LInt x), VLit (LFloat y) -> VLit (LBool (op_float (float x) y))
+    | VLit (LFloat x), VLit (LInt y) -> VLit (LBool (op_float x (float y)))
+    | _ -> unexpected_error "eval_expr: illegal operands in binary operator (%s): %s %s %s" (op) (pretty_value v1) (op) (pretty_value v2)
+
+and binop_logic op_bool op env e1 e2 =
+    let v1 = eval_expr env e1
+    let v2 = eval_expr env e2
+    match v1, v2 with
+    | VLit (LBool x), VLit (LBool y) -> VLit (LBool (op_bool x y))
+    | _ -> unexpected_error "eval_expr: illegal operands in binary operator (%s): %s %s %s" (op) (pretty_value v1) (op) (pretty_value v2)
---
+++ Typing.fs
@@ -0,0 +1,310 @@
+﻿(*
+* TinyML
+* Typing.fs: typing algorithms
+*)
+
+module TinyML.Typing
+
+open Ast
+
+let type_error fmt = throw_formatted TypeError fmt
+
+type subst = (tyvar * ty) list
+
+// type inference
+//
+
+let gamma0 = [
+    ("+", TyArrow (TyInt, TyArrow (TyInt, TyInt)))
+    ("-", TyArrow (TyInt, TyArrow (TyInt, TyInt)))
+    ("*", TyArrow (TyInt, TyArrow (TyInt, TyInt)))
+    ("/", TyArrow (TyInt, TyArrow (TyInt, TyInt)))
+    ("%", TyArrow (TyInt, TyArrow (TyInt, TyInt)))
+    ("=", TyArrow (TyInt, TyArrow (TyInt, TyBool)))
+    ("<", TyArrow (TyInt, TyArrow (TyInt, TyBool)))
+    ("<=", TyArrow (TyInt, TyArrow (TyInt, TyBool)))
+    (">", TyArrow (TyInt, TyArrow (TyInt, TyBool)))
+    ("=>", TyArrow (TyInt, TyArrow (TyInt, TyBool)))
+    ("<>", TyArrow (TyInt, TyArrow (TyInt, TyBool)))
+    ("and", TyArrow (TyBool, TyArrow (TyBool, TyBool)))
+    ("or", TyArrow (TyBool, TyArrow (TyBool, TyBool)))
+    ("not", TyArrow (TyBool, TyBool))
+    ("-", TyArrow (TyInt, TyInt))
+]
+
+let counter = ref -1
+
+let generate_fresh_variable () : string =
+    let list_variables = ['a' .. 'z'] |> List.map string
+    counter.Value <- !counter + 1
+    list_variables.Item(!counter)
+
+let rec occurs (tv : tyvar) (t : ty) : bool = 
+    match t with
+    | TyVar t1 -> tv = t1
+    | TyArrow (t1,t2) -> occurs tv t1 || occurs tv t2
+    | TyName t1 -> false
+    | TyTuple tt -> let rec occ_list (tv : tyvar) (t : ty list) : bool =
+                        match t with
+                        |[] -> false
+                        |head::tail -> if occurs tv head
+                                       then true
+                                       else occ_list tv tail
+                    occ_list tv tt
+
+// TODO implement this
+let compose_subst (s1 : subst) (s2 : subst) : subst = s1 @ s2
+
+// TODO implement this
+let rec unify (t1 : ty) (t2 : ty) : subst = 
+    match t1, t2 with
+    | TyName n1, TyName n2 -> if n1 <> n2 
+                              then type_error "unify: unification between different variables name can't be execute"
+                              else []
+    | TyVar tv, _ -> if occurs tv t2 
+                     then type_error "unify: unification fails"
+                     else [(tv , t2)]
+
+    | _ , TyVar tv -> if occurs tv t1 
+                      then type_error "unify: unification fails"
+                      else [(tv , t1)]
+
+    | TyArrow (tl1,tr1), TyArrow (tl2,tr2) ->   let u1 = unify tl1 tl2
+                                                let u2 = unify tr1 tr2
+                                                compose_subst u1 u2
+
+                                                (*let subs1 = unify tl1 tl2
+                                                let te1 = apply_subst tr1 subs1 
+                                                let te2 = apply_subst tr2 subs1 
+                                                let subs2 = unify te1 te2
+                                                compose_subst subs1 subs2*)
+    | _ -> unexpected_error "unify: unsupported operation"
+
+(* substitute term s for all occurrences of var x in term t *)
+let rec subst (s : ty) (x : tyvar) (t : ty) : ty =
+  match t with
+  | TyVar y -> if x = y then s else t
+  | TyArrow (u, v) -> TyArrow (subst s x u, subst s x v)
+  | TyName n -> t
+  | TyTuple ts -> TyTuple(List.map (subst s x) ts)
+
+// TODO implement this
+let apply_subst (t : ty) (s : subst) : ty = 
+  List.foldBack (fun (x, e) -> subst e x) s t
+
+let apply_subst_helper s t = apply_subst t s
+
+// Give all tyvar in a type
+let rec freevars_ty (t : ty) : tyvar Set =
+    match t with
+    | TyName _ -> Set.empty
+    | TyArrow (t1, t2) -> Set.union (freevars_ty t1) (freevars_ty t2)
+    | TyVar tv -> Set.singleton tv
+    | TyTuple ts -> List.fold (fun set t -> Set.union set (freevars_ty t)) Set.empty ts 
+
+let freevars_scheme (Forall (tvs, t)) =
+    Set.difference (freevars_ty t) (Set.ofList tvs)
+
+
+let rec tupleMap l: subst =
+    match l with
+    |[] -> []
+    |head::tail -> 
+                   match head with
+                   |(_,su) -> compose_subst su (tupleMap tail)
+
+let rec tupleMap2 l: ty list =
+    match l with
+    |[] -> []
+    |head::tail -> 
+                   match head with
+                   |(typ,_) -> typ::(tupleMap2 tail)
+// type inference
+//
+
+let rec typeinfer_expr (env : ty env) (e : expr) : ty * subst =
+    match e with
+    | Var x -> 
+        let _, t = List.find (fun (y, _) -> x = y) env
+        (t, [])
+    | Lit (LInt _) -> (TyInt, [])
+    | Lit (LFloat _) -> (TyFloat, [])
+    | Lit (LString _) -> (TyString, [])
+    | Lit (LChar _) -> (TyChar, [])
+    | Lit (LBool _) -> (TyBool, [])
+    | Lit LUnit -> (TyUnit, [])
+
+    | App (e1, e2) ->
+        let codTy = TyVar(generate_fresh_variable ())
+        let t1, s1 = typeinfer_expr env e1
+        let t2, s2 = typeinfer_expr env e2
+        let s3 = unify t1 (TyArrow (t2,codTy))
+        let s32 = compose_subst s3 s2 
+        let s321 = compose_subst s32 s1
+        (apply_subst codTy s321, s321) 
+
+    | Lambda (x, None, e) ->
+        let freshVar = TyVar(generate_fresh_variable())
+        let t,s = typeinfer_expr((x, freshVar) :: env) e
+        //final type = 'a -> bodyTypeCheckType with the substitution produced by bodyTypeCheckType
+        let finalType = apply_subst (TyArrow(freshVar,t)) s
+        (finalType,s)
+
+    | Lambda (x, Some typ, e) ->
+        let t,s = typeinfer_expr((x, typ) :: env) e
+        //final type = 'a -> bodyTypeCheckType with the substitution produced by bodyTypeCheckType
+        let finalType = apply_subst (TyArrow(typ,t)) s
+        (finalType,s)
+
+    //monomorphic version
+    | Let (x, None , e1, e2) -> 
+        let t1, s1 = typeinfer_expr env e1
+        let t2, s2 = typeinfer_expr ((x,t1) :: env) e2
+        let s3 = compose_subst s2 s1
+        (t2, s3)
+
+    | IfThenElse (e1, e2, e3o) ->
+        let t1, s1 = typeinfer_expr env e1
+        let t2, s2 = typeinfer_expr env e2
+        let s4 = unify t1 TyBool
+        match e3o with
+        | None -> let s5 = unify t2 TyUnit
+                  let tot = compose_subst (compose_subst (compose_subst s5 s4) s2) s1
+                  (apply_subst t2 s5, tot)
+
+        | Some ex -> let t3, s3 = typeinfer_expr env ex
+                     let s5 = unify t2 t3
+                     let tot = compose_subst (compose_subst (compose_subst (compose_subst s5 s4) s3) s2) s1
+                     (apply_subst t2 s5, tot)
+
+    | Tuple es ->
+        let t = List.map (typeinfer_expr env) es
+        let comp = tupleMap t
+        let typL =  tupleMap2 t
+        let typ = TyTuple(List.map (apply_subst_helper comp) typL)
+        (typ,comp)
+
+    | BinOp(e1, op, e2) ->
+        typeinfer_expr env (App (App (Var op, e1), e2))
+
+    | BinOp (_, op, _) -> unexpected_error "typecheck_expr: unsupported binary operator (%s)" op
+        
+    | UnOp(op, e) ->
+        typeinfer_expr env (App (Var op, e))
+
+    | UnOp (op, _) -> unexpected_error "typeinfer_expr: unsupported unary operator (%s)" op
+
+    | _ -> unexpected_error "typeinfer_expr: unsupported expression: %s [AST: %A]" (pretty_expr e) e
+
+// type checker
+//
+    
+let rec typecheck_expr (env : ty env) (e : expr) : ty =
+    match e with
+    | Lit (LInt _) -> TyInt
+    | Lit (LFloat _) -> TyFloat
+    | Lit (LString _) -> TyString
+    | Lit (LChar _) -> TyChar
+    | Lit (LBool _) -> TyBool
+    | Lit LUnit -> TyUnit
+
+    | Var x ->
+        let _, t = List.find (fun (y, _) -> x = y) env
+        t
+
+    | Lambda (x, None, e) -> unexpected_error "typecheck_expr: unannotated lambda is not supported"
+    
+    | Lambda (x, Some t1, e) ->
+        let t2 = typecheck_expr ((x, t1) :: env) e
+        TyArrow (t1, t2)
+
+    | App (e1, e2) ->
+        let t1 = typecheck_expr env e1
+        let t2 = typecheck_expr env e2
+        match t1 with
+        | TyArrow (l, r) ->
+            if l = t2 then r 
+            else type_error "wrong application: argument type %s does not match function domain %s" (pretty_ty t2) (pretty_ty l)
+        | _ -> type_error "expecting a function on left side of application, but got %s" (pretty_ty t1)
+
+    | Let (x, tyo, e1, e2) ->
+        let t1 = typecheck_expr env e1
+        match tyo with
+        | None -> ()
+        | Some t -> if t <> t1 then type_error "type annotation in let binding of %s is wrong: exptected %s, but got %s" x (pretty_ty t) (pretty_ty t1)
+        typecheck_expr ((x, t1) :: env) e2
+
+    | IfThenElse (e1, e2, e3o) ->
+        let t1 = typecheck_expr env e1
+        if t1 <> TyBool then type_error "if condition must be a bool, but got a %s" (pretty_ty t1)
+        let t2 = typecheck_expr env e2
+        match e3o with
+        | None ->
+            if t2 <> TyUnit then type_error "if-then without else requires unit type on then branch, but got %s" (pretty_ty t2)
+            TyUnit
+        | Some e3 ->
+            let t3 = typecheck_expr env e3
+            if t2 <> t3 then type_error "type mismatch in if-then-else: then branch has type %s and is different from else branch type %s" (pretty_ty t2) (pretty_ty t3)
+            t2
+
+    | Tuple es ->
+        TyTuple (List.map (typecheck_expr env) es)
+
+    | LetRec (f, None, e1, e2) ->
+        unexpected_error "typecheck_expr: unannotated let rec is not supported"
+        
+    | LetRec (f, Some tf, e1, e2) ->
+        let env0 = (f, tf) :: env
+        let t1 = typecheck_expr env0 e1
+        match t1 with
+        | TyArrow _ -> ()
+        | _ -> type_error "let rec is restricted to functions, but got type %s" (pretty_ty t1)
+        if t1 <> tf then type_error "let rec type mismatch: expected %s, but got %s" (pretty_ty tf) (pretty_ty t1)
+        typecheck_expr env0 e2
+
+    | BinOp (e1, ("+" | "-" | "/" | "%" | "*" as op), e2) ->
+        let t1 = typecheck_expr env e1
+        let t2 = typecheck_expr env e2
+        match t1, t2 with
+        | TyInt, TyInt -> TyInt
+        | TyFloat, TyFloat -> TyFloat
+        | TyInt, TyFloat
+        | TyFloat, TyInt -> TyFloat
+        | _ -> type_error "binary operator expects two int operands, but got %s %s %s" (pretty_ty t1) op (pretty_ty t2)
+        
+    | BinOp (e1, ("<" | "<=" | ">" | ">=" | "=" | "<>" as op), e2) ->
+        let t1 = typecheck_expr env e1
+        let t2 = typecheck_expr env e2
+        match t1, t2 with
+        | TyInt, TyInt
+        | TyFloat, TyFloat
+        | TyInt, TyFloat
+        | TyFloat, TyInt -> ()
+        | _ -> type_error "binary operator expects two numeric operands, but got %s %s %s" (pretty_ty t1) op (pretty_ty t2)
+        TyBool
+
+    | BinOp (e1, ("and" | "or" as op), e2) ->
+        let t1 = typecheck_expr env e1
+        let t2 = typecheck_expr env e2
+        match t1, t2 with
+        | TyBool, TyBool -> ()
+        | _ -> type_error "binary operator expects two bools operands, but got %s %s %s" (pretty_ty t1) op (pretty_ty t2)
+        TyBool
+
+    | BinOp (_, op, _) -> unexpected_error "typecheck_expr: unsupported binary operator (%s)" op
+
+    | UnOp ("not", e) ->
+        let t = typecheck_expr env e
+        if t <> TyBool then type_error "unary not expects a bool operand, but got %s" (pretty_ty t)
+        TyBool
+            
+    | UnOp ("-", e) ->
+        let t = typecheck_expr env e
+        match t with
+        | TyInt -> TyInt
+        | TyFloat -> TyFloat
+        | _ -> type_error "unary negation expects a numeric operand, but got %s" (pretty_ty t)
+
+    | UnOp (op, _) -> unexpected_error "typecheck_expr: unsupported unary operator (%s)" op
+
+    | _ -> unexpected_error "typecheck_expr: unsupported expression: %s [AST: %A]" (pretty_expr e) e
